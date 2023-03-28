224f2e0b4f76a7c5c9fa67d71be9eec0e9958bfe
Gavin Vaske <Gavin.Vaske@gmail.com>
2023-03-28 14:09:24 -0500

    add/update TransactionList and Transaction components; install axios to talk with server

--- client/components/Transaction/Transaction.js
+++ client/components/Transaction/Transaction.js
@@ -1,6 +1,12 @@
 import React from 'react';
 
 export default function Transaction(props) {
-    return (<div>TODO: Transaction.js</div>
-    );
+  const {description, price} = props;
+
+  return (
+    <div id='transaction'>
+      <p>Description: {description}</p>
+      <p>Price: ${price}</p>
+    </div>
+  );
 }
--- client/components/TransactionList/TransactionList.js
+++ client/components/TransactionList/TransactionList.js
@@ -1,6 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Transaction from '../Transaction/Transaction';
 
 export default function TransactionList(props) {
-    return (<div>TODO: TransactionList.js</div>
-    );
+  const [transactions, setTransactions] = useState([]);
+
+  async function getTransactions() {
+    const transactionsFromDatabase = await axios.get('http://localhost:8080/transactions')
+
+    setTransactions(transactionsFromDatabase.data);
+  }
+
+  useEffect(() => {
+    getTransactions();
+  }, [])
+
+  return (
+    <div id='transaction-list'>
+      {transactions && transactions.map((transaction) => <Transaction {...transaction} />)}\
+    </div>
+  );
 }
